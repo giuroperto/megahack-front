f2b37f41f7a764cce8e4f87530ca0a986cec1b3c
Fernando Lima Borrelli <flborrelli@gmail.com>
2020-07-04 18:43:22 -0300

    User profile component done

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,8 @@ import Home from './components/Home/Home';
 import CreateDiscounts from './components/CreateDiscounts/CreateDiscounts';
 import Discounts from './components/Discounts/Discounts';
 import CreateRestaurants from './components/CreateRestaurants/CreateRestaurants';
+import UserProfile from './components/UserProfile/UserProfile';
+
 
 const Routes = () => {
   return(
@@ -13,6 +15,7 @@ const Routes = () => {
         <Route path='/create-discounts' component={CreateDiscounts} />
         <Route path='/discounts' component={Discounts} />
         <Route path='/create-restaurants' component={CreateRestaurants} />
+        <Route path='/user-profile' component={UserProfile} />
       </Switch>
     </BrowserRouter>
   )
