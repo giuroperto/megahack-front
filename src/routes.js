b9f4754990508540f87847c123d6270a3d09d7f9
Giu Roperto <giu.roperto@gmail.com>
2020-07-04 16:00:56 -0300

    social login structure

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './components/Home/Home';
+import SocialLogin from './components/SocialLogin/SocialLogin';
 
 const Routes = () => {
   return(
     <BrowserRouter>
       <Switch>
         <Route path='/' exact component={Home} />
+        <Route path='/login' exact component={SocialLogin} />
       </Switch>
     </BrowserRouter>
   )
