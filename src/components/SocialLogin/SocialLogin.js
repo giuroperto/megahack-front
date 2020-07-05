70b80df32300d753866e71aad1bc0aabf5e450bb
Giu Roperto <giu.roperto@gmail.com>
2020-07-04 21:43:11 -0300

    deleted config json

--- src/components/SocialLogin/SocialLogin.js
+++ src/components/SocialLogin/SocialLogin.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import FacebookLogin from 'react-facebook-login';
 import { GoogleLogin } from 'react-google-login';
-import config from '../../config.json';
 
 class Home extends Component {
   constructor(props){
