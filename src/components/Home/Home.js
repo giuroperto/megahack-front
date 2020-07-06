576d7bc9d5d953acd84f3b5d768faa131317f3de
Giu Roperto <giu.roperto@gmail.com>
2020-07-05 23:40:17 -0300

    invisible links

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 const Home = () => {
   return(
       <div className="home-container">
+        <Link to='/create-discounts' className='invisible-link'>CREATE DISCOUNT</Link>
         <h1>Thykhe</h1>
         <h2>A responsável pela volta da prosperidade ao seu estabelecimento!</h2>
         <Link to='/login'>login</Link>
